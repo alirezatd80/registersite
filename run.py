e77a3e94b19f01d6f501bfe2a3f452f6bc0a876f
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-23 02:20:12 +0330

    declaring login page

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template,redirect,request,url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def main():
     return 'hi'
 
+@app.route("/log")
+def login():
+    return render_template('login.html')
+    
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
     
