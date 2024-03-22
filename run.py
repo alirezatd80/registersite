21f1b3e3c61c373d5131d8dcbeaf0f6d56bf8450
Alireza <153272155+alirezatd80@users.noreply.github.com>
2024-03-23 03:15:44 +0330

    add some comments

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template,redirect,request,url_for
 from flask_sqlalchemy import SQLAlchemy
 
-
+#setting
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///admins.db'
 db = SQLAlchemy(app)
 
+#models
 class Admins(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -14,15 +15,19 @@ class Admins(db.Model):
     def __repr__(self):
         return f"Admin('{self.username}')"
 
+#create database file
 with app.app_context():
       
        db.create_all()
-       
+
+#database functions 
 def AddAdmin(name , passsword):
     admin = Admins(username = name , password=passsword)
     db.session.add(admin)
     db.session.commit()
 
+
+# routes
 @app.route("/")
 def main():
     return 'hi'
@@ -36,6 +41,8 @@ def adminpage():
     return render_template('adminpage.html')
 
 
+
+#run app
 if __name__ == "__main__":
     app.run(debug=True)
     
